1caf44039f89d948dc729e4feec2339789279714
reishin56-dot <reishin56@googlemail.com>
2026-03-20 09:43:52 +0100

    + - for Food & Drinks
    functionality to increase or decrease Food&Drinks

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ function goToMenu() {
     return;
   }
 
-  // Weiterleitung zur neuen Seite
   window.location.href = "menu.html";
 }
 
@@ -16,4 +15,40 @@ function goBack() {
 
 function goToPage(page) {
   window.location.href = page;
+}
+
+
+// Preise
+let prices = {
+  wasser: 5,
+  cocktail: 12,
+  bier: 8
+};
+
+
+// Menge ändern
+function change(item, delta) {
+  let el = document.getElementById(item);
+  let value = parseInt(el.innerText);
+
+  value += delta;
+
+  // Minimum = 0
+  if (value < 0) value = 0;
+
+  el.innerText = value;
+  updateTotal();
+}
+
+
+//NEU: Gesamt berechnen
+function updateTotal() {
+  let total = 0;
+
+  for (let item in prices) {
+    let amount = parseInt(document.getElementById(item).innerText);
+    total += amount * prices[item];
+  }
+
+  document.getElementById("total").innerText = total;
 }
